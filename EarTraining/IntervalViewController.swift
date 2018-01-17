506dc022eb5263be0e11f80ad6162c6272d0317f
atodoki@gmail.com <atodoki@gmail.com>
2018-01-16 23:45:52 -0800

    Add AudioKit Framework, add simple labels and buttons to 2 viewControllers

---
+++ EarTraining/IntervalViewController.swift
@@ -0,0 +1,35 @@
+//
+//  IntervalViewController.swift
+//  EarTraining
+//
+//  Created by Ariel Todoki on 1/16/18.
+//  Copyright © 2018 Ariel Todoki. All rights reserved.
+//
+
+import UIKit
+
+class IntervalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
