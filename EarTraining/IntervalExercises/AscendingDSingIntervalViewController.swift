0dbdd27d7aa71e279a2276608d7a0ca9c0071efd
atodoki@gmail.com <atodoki@gmail.com>
2018-04-06 22:19:32 -0700

    Add instruments to exercise, delete low range and high range sliders, added slider to change the octave

--- EarTraining/IntervalExercises/AscendingDSingIntervalViewController.swift
+++ EarTraining/IntervalExercises/AscendingDSingIntervalViewController.swift
@@ -16,23 +16,19 @@ class AscendingDSingIntervalViewController: UIViewController {
     @IBOutlet private var sungNote: UILabel!
     @IBOutlet private var intervalLabel: UILabel!
     @IBOutlet private var recordButton: UIButton!
-    @IBOutlet private var lowestNoteLabel: UILabel!
-    @IBOutlet private var highestNoteLabel: UILabel!
     
-
-    var givenNoteIndex = 0
-    var givenNoteFreq = 0.0
-    
-    var lowOctave = 3.0
-    var highOctave = 5.0
-    var answerOctave = 0.0
+    var bNoteOctave = 4
+    var tNoteOctave = 4
+    var bottomNote = 0
+    var topNote = 0
+    var intervalSize = 0
     var intervalIndex = 0
-    var answerNoteFreq = 0.0
+    //var randomIndex = 0
     
-    var lowValueIndex = 17
-    var highValueIndex = 12
+    let sampler = AKSampler()
+    var timePitch: AKTimePitch!
     
-    let oscillator = AKOscillator()
+    let soundNames = ["Kawai-K11-GrPiano-C4", "Ensoniq-SQ-1-Clarinet-C4", "Ensoniq-SQ-1-French-Horn-C4", "Alesis-Fusion-Pizzicato-Strings-C4"]
     
     var mic: AKMicrophone!
     var tracker: AKFrequencyTracker!
@@ -41,6 +37,10 @@ class AscendingDSingIntervalViewController: UIViewController {
     
     
     let noteFrequencies = [16.35, 17.32, 18.35, 19.45, 20.6, 21.83, 23.12, 24.5, 25.96, 27.5, 29.14, 30.87]
+    
+    let noteCents = [0.0, 100.0, 200.0, 300.0, 400.0, 500.0, 600.0, 700.0, 800.0, 900.0, 1000.0, 1100.0]
+    let octaveChange = [-6000.0,-3600.0,-2400.0,-1200.0,0,1200.0,2400.0]
+    
     let noteNamesWithSharps = ["C", "C♯", "D", "D♯", "E", "F", "F♯", "G", "G♯", "A", "A♯", "B"]
     let noteNamesWithFlats = ["C", "D♭", "D", "E♭", "E", "F", "G♭", "G", "A♭", "A", "B♭", "B"]
     
@@ -52,30 +52,46 @@ class AscendingDSingIntervalViewController: UIViewController {
     
     let intervalNames = ["Major 2nd", "Major 3rd", "Perfect 4th", "Perfect 5th", "Major 6th", "Major 7th", "Octave"]
     
-    let lowRangeSlider = ["C2", "C#2", "D2", "D#2", "E2", "F2", "F#2", "G2","G#2", "A2", "A#2", "B2", "C3","C#3", "D3", "D#3", "E3", "F3", "F#3","G3", "G#3", "A3", "A#3","B3"]
     
-    let highRangeSlider = ["C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4","G#4", "A4", "A#4", "B4", "C5","C#5", "D5", "D#5", "E5", "F5", "F#5","G5", "G#5", "A5", "A#5","B5"]
-    
-    let intervalDiff = [2, 4, 5, 7, 9, 11, 12]
+    let diatonicIntervals = [2, 4, 5, 7, 9, 11, 12]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        try! sampler.loadWav("../\(soundNames[0])")
+        
+        timePitch = AKTimePitch(sampler)
+        timePitch.rate = 2.0
+        timePitch.pitch = 0.0
+        timePitch.overlap = 8.0
+        
+        
         AKSettings.audioInputEnabled = true
         mic = AKMicrophone()
         tracker = AKFrequencyTracker(mic)
         silence = AKBooster(tracker, gain: 0)
-        //AudioKit.output = silence
+        AudioKit.output = silence
+  
+        AudioKit.output = timePitch
         
-        oscillator.frequency = 0.0
-        oscillator.amplitude = 0.5
-        AudioKit.output = oscillator
-        
-        
+        setInterval()
 
     }
     
+    
+    func setInterval(){
+        bottomNote = Int(arc4random_uniform(12)) // random number 0<n<12-1
+        intervalIndex = diatonicIntervals.randomIndex
+        intervalSize = diatonicIntervals[intervalIndex]
+        topNote = bottomNote + intervalSize
+        tNoteOctave = (topNote > 11 ? bNoteOctave + 1 : bNoteOctave)
+        
+        givenNote.text = "\(noteNamesWithSharps[bottomNote])\(bNoteOctave)"
+        intervalLabel.text = "Sing \(intervalNames[intervalIndex])"
+        
+    }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
@@ -88,119 +104,51 @@ class AscendingDSingIntervalViewController: UIViewController {
         AudioKit.stop()
     }
     
-    @IBAction func changeHighNote(sender: UISlider) {
-        highValueIndex = Int(sender.value)
-        highestNoteLabel.text = "Highest Note: \(highRangeSlider[highValueIndex])"
-        
-        if(highValueIndex < 12){
-            highOctave = 4.0
-        }
-        else {
-            highOctave = 5.0
-        }
     
-    }
-    
-    @IBAction func changeLowNote(sender: UISlider){
-        lowValueIndex = Int(sender.value)
-        lowestNoteLabel.text = "Lowest Note: \(lowRangeSlider[lowValueIndex])"
-        
-        if (lowValueIndex < 12) {
-            lowOctave = 2.0
-        }
-        else{
-            lowOctave = 3.0
-        }
+    @IBAction func changeOctave(sender: UISlider){
+        bNoteOctave = Int(sender.value)
+        tNoteOctave = topNote > 11 ? bNoteOctave + 1 : bNoteOctave
+        givenNote.text = "\(noteNamesWithSharps[bottomNote])\(bNoteOctave)"
     }
     
     
     @IBAction func replay(sender: UIButton){
-        AudioKit.output = oscillator
-        oscillator.frequency = givenNoteFreq
-        oscillator.start()
-        sleep(2)
-        oscillator.stop()
+        timePitch.pitch = noteCents[bottomNote] + octaveChange[bNoteOctave]
+        sampler.play()
+        sleep(1)
+
     }
     
     @IBAction func playAnswer(sender: UIButton){
-        AudioKit.output = oscillator
-        oscillator.frequency = answerNoteFreq
-        oscillator.start()
-        sleep(2)
-        oscillator.stop()
+        timePitch.pitch = noteCents[topNote%12] + octaveChange[tNoteOctave]
+        sampler.play()
+        sleep(1)
+
     }
     
     @IBAction func start(sender: UIButton){
-        AudioKit.output = oscillator
         
         // reset label background colors
         sungNote.backgroundColor = UIColor.white
+        sungNote.text = "Sung Note"
         givenNote.backgroundColor = UIColor.white
-        
-        // finds note that is not below the lowest note, or not in the same octave or higher than high octave range if it were pushed up an octave
-        repeat{
-            givenNoteIndex = Int(arc4random_uniform(12))
-        }while(givenNoteIndex < lowValueIndex%12)&&(lowOctave+1 >= highOctave)
-
-        // Play note within user specified range
-        var currLowOctave = lowOctave
-        if(givenNoteIndex < lowValueIndex%12){
-            currLowOctave = lowOctave + 1
-        }else{
-            currLowOctave = lowOctave
-        }
-        givenNoteFreq = noteFrequencies[givenNoteIndex] * pow(2,currLowOctave)
-        givenNote.text = "\(noteNamesWithSharps[givenNoteIndex])\(Int(currLowOctave))"
-        
-        
-        playDiatonicInterval(currLowOctave: currLowOctave)
-        
-
-    }
-
-    func playDiatonicInterval(currLowOctave: Double){
-    // pick random diatonic interval
-    var intDiff = 0
-    intervalIndex = 0
-    repeat{
-        intervalIndex = Int(arc4random_uniform(7))
-        intDiff = intervalDiff[intervalIndex]
-        print(intDiff)
-        
-    } while (givenNoteIndex + intDiff > 11)&&((givenNoteIndex+intDiff)%12 > highValueIndex%12)&&((currLowOctave+1 == highOctave)||currLowOctave == highOctave)&&((lowValueIndex < 22)||(highValueIndex != 0)) // keep picking a random interval until it is in the range specified by user
     
-    if(lowValueIndex < 22 || highValueIndex != 0){ // checks if interval range specified by user is too small
-        intervalLabel.text = "Sing \(intervalNames[intervalIndex]) above"
+        setInterval()
         
-        //calculate answer
-        if((givenNoteIndex + intDiff) > 11){
-            answerNoteFreq = noteFrequencies[(givenNoteIndex + intDiff)%12] * pow(2,currLowOctave+1)
-            answerOctave = currLowOctave+1
-        }else {
-            answerNoteFreq = noteFrequencies[givenNoteIndex + intDiff] * pow(2,currLowOctave)
-            answerOctave = currLowOctave
-        }
-        
-        // play given note
-        oscillator.frequency = givenNoteFreq
-        oscillator.start()
-        sleep(2)
-        oscillator.stop()
+        timePitch.pitch = noteCents[bottomNote] + octaveChange[bNoteOctave]
+        sampler.play()
+
     }
-    else{
-        intervalLabel.text = "Interval Range too small"
-    }
-    
-}
+
     
     
     @objc func updateUI(){
-        AudioKit.output = silence
+        //AudioKit.output = silence
         var frequency0 = tracker.frequency
         var sungOctave = 0.0
         var sungNoteIndex = 0;
-        print(mic.isStarted)
-        print(tracker.amplitude)
+//        print(mic.isStarted)
+//        print(tracker.amplitude)
         if(tracker.amplitude > 0.09){
             
             // Get note frequency in octave 0
@@ -222,8 +170,8 @@ class AscendingDSingIntervalViewController: UIViewController {
             sungNote.text = "\(noteNamesWithSharps[sungNoteIndex])\(Int(sungOctave))"
             
             //checks if sung interval is correct
-            let ansIndex = (givenNoteIndex + intervalDiff[intervalIndex])%12
-            if((ansIndex == sungNoteIndex) && (sungOctave == answerOctave)){ // if correct pitch class and octave
+
+            if((sungNoteIndex == topNote%12) && (Int(sungOctave) == tNoteOctave)){ // if correct pitch class and octave
                     if(frequency0 > inTuneLow[sungNoteIndex] && frequency0 < inTuneHigh[sungNoteIndex]){ // checks if sung note is within -10 and +10 cents of correct pitch
                     sungNote.backgroundColor = UIColor.green
                     }else{
@@ -237,17 +185,13 @@ class AscendingDSingIntervalViewController: UIViewController {
     
     @IBAction func handleGesture(_ sender: UILongPressGestureRecognizer) {
         if sender.state == UIGestureRecognizerState.began{
-//            tracker.start()
-//            mic.start()
             
             timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(AscendingDSingIntervalViewController.updateUI), userInfo: nil, repeats: true)
             recordButton.setTitle("Listening...", for: .normal)
         }
         else if sender.state == UIGestureRecognizerState.ended{
-//            tracker.stop()
             timer.invalidate()
-//            mic.stop()
-            recordButton.setTitle("Press to Record", for: .normal)
+            recordButton.setTitle("Hold to Record", for: .normal)
         }
     }
 
