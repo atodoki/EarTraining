b80d2b98f8b6077b4b1f2c84db70cbec3f9811f7
atodoki@gmail.com <atodoki@gmail.com>
2018-04-08 01:12:39 -0700

    Add descending chromatic view controller with exercise implemented, change color of view controllers

--- EarTraining/IntervalExercises/DescendingChromaticViewController.swift
+++ EarTraining/IntervalExercises/DescendingChromaticViewController.swift
@@ -1,10 +1,8 @@
-
-
 //
-//  DescendingDiatonicViewController.swift
+//  DescendingChromaticViewController.swift
 //  EarTraining
 //
-//  Created by Ariel Todoki on 3/1/18.
+//  Created by Ariel Todoki on 4/7/18.
 //  Copyright © 2018 Ariel Todoki. All rights reserved.
 //
 
@@ -12,36 +10,38 @@ import UIKit
 import AudioKit
 import AudioKitUI
 
-class DescendingDiatonicViewController: UIViewController {
+class DescendingChromaticViewController: UIViewController {
 
     @IBOutlet var intervalButtons: [UIButton]!
     @IBOutlet var exerciseNumLabel: UILabel!
     
-    let noteCents = [0.0, 100.0, 200.0, 300.0, 400.0, 500.0, 600.0, 700.0, 800.0, 900.0, 1000.0, 1100.0]
-    let octaveChange = [-6000.0,-3600.0,-2400.0,-1200.0,0,1200.0,2400.0]
-    
-    let diatonicIntervals = [2,4,5,7,9,11,12]
+    let noteFrequency = [16.35, 17.32, 18.35, 19.45, 20.6, 21.83, 23.12, 24.5, 25.96, 27.5, 29.14, 30.87]
+    let noteNameSharps = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+    let noteNameFlats = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"]
     
     var bNoteOctave = 4
     var tNoteOctave = 4
     
     var bottomNote = 0
     var topNote = 0
-    var randomIndex = 0
-    
-    var exerciseNum = 1
-    
+    var intervalSize = 0
+    var exerciseNum = 1;
     
     let sampler = AKSampler()
     var timePitch: AKTimePitch!
+    
+    let noteCents = [0.0, 100.0, 200.0, 300.0, 400.0, 500.0, 600.0, 700.0, 800.0, 900.0, 1000.0, 1100.0]
+    
+    let octaveChange = [-6000.0,-3600.0,-2400.0,-1200.0,0,1200.0,2400.0]
     
     let soundNames = ["Kawai-K11-GrPiano-C4", "Ensoniq-SQ-1-Clarinet-C4", "Ensoniq-SQ-1-French-Horn-C4", "Alesis-Fusion-Pizzicato-Strings-C4"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        try! sampler.loadWav("../\(soundNames[0])")
+        // Do any additional setup after loading the view.
         
+        try! sampler.loadWav("../\(soundNames[0])")
         
         timePitch = AKTimePitch(sampler)
         timePitch.rate = 2.0
@@ -54,17 +54,16 @@ class DescendingDiatonicViewController: UIViewController {
         
     }
     
-    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         AudioKit.start()
         
     }
- 
+    
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         AudioKit.stop()
-        
     }
     
     override func didReceiveMemoryWarning() {
@@ -72,10 +71,12 @@ class DescendingDiatonicViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK: - Defined Functions
+    
     func setInterval(){
         topNote = Int(arc4random_uniform(12)) // random number 0<n<12-1
-        randomIndex = Int(arc4random_uniform(UInt32(diatonicIntervals.count))) // random number from array diatonicIntervals
-        bottomNote = topNote - diatonicIntervals[randomIndex]// random number diatonic interval from topNote
+        intervalSize = Int(arc4random_uniform(11))+1 // random number 1<n<11
+        bottomNote = topNote - intervalSize // random number chromatic interval from top note
         tNoteOctave = Int(arc4random_uniform(3))+3
         bNoteOctave = (bottomNote < 0 ? tNoteOctave - 1 : tNoteOctave)
         bottomNote += 12
@@ -87,12 +88,11 @@ class DescendingDiatonicViewController: UIViewController {
         sleep(1)
         
         timePitch.pitch = noteCents[bottomNote%12] + octaveChange[bNoteOctave]
-        
         sampler.play()
     }
     
     func checkAnswer(sender: UIButton, interval: Int){
-        if(diatonicIntervals[randomIndex] == interval){
+        if(intervalSize == interval){
             sender.backgroundColor = UIColor.green
             for b in intervalButtons{
                 b.isEnabled = false
@@ -123,12 +123,10 @@ class DescendingDiatonicViewController: UIViewController {
     
     @IBAction func playAgain(sender: UIButton){
         playInterval()
-        
     }
     
     @IBAction func next(sender: UIButton){
-        
-        // MAKE BUTTON BACKGROUNDS GO BACK TO NORMAL
+        // Reset interval button background color
         for b in intervalButtons{
             b.backgroundColor = UIColor.white
             b.isEnabled = true
@@ -143,47 +141,77 @@ class DescendingDiatonicViewController: UIViewController {
     }
     
     
+    @IBAction func minorSecond(sender: UIButton){
+        // Button turns green if the interval played is a minor second, turns red if not
+        checkAnswer(sender: sender, interval: 1)
+    }
+    
     @IBAction func majorSecond(sender: UIButton){
         // Button turns green if the interval played is a major second, turns red if not
         checkAnswer(sender: sender, interval: 2)
-
+        
     }
-
+    
+    @IBAction func minorThird(sender: UIButton){
+        // Button turns green if the interval played is a minor third, turns red if not
+        checkAnswer(sender: sender, interval: 3)
+        
+    }
+    
     @IBAction func majorThird(sender: UIButton){
         // Button turns green if the interval played is a major third, turns red if not
         checkAnswer(sender: sender, interval: 4)
-
+        
     }
-
+    
     @IBAction func perfectFourth(sender: UIButton){
         // Button turns green if the interval played is a perfect fourth, turns red if not
         checkAnswer(sender: sender, interval: 5)
-
+        
     }
-
+    
+    @IBAction func tritone(sender: UIButton){
+        // Button turns green if the interval played is a tritone, turns red if not
+        checkAnswer(sender: sender, interval: 6)
+        
+    }
+    
     @IBAction func perfectFifth(sender: UIButton){
         // Button turns green if the interval played is a perfect fifth, turns red if not
         checkAnswer(sender: sender, interval: 7)
-
+        
     }
-
+    
+    @IBAction func minorSixth(sender: UIButton){
+        // Button turns green if the interval played is a minor sixth, turns red if not
+        checkAnswer(sender: sender, interval: 8)
+        
+    }
+    
     @IBAction func majorSixth(sender: UIButton){
         // Button turns green if the interval played is a major sixth, turns red if not
         checkAnswer(sender: sender, interval: 9)
+        
     }
-
+    
+    @IBAction func minorSeventh(sender: UIButton){
+        // Button turns green if the interval played is a minor seventh, turns red if not
+        checkAnswer(sender: sender, interval: 10)
+        
+    }
+    
     @IBAction func majorSeventh(sender: UIButton){
         // Button turns green if the interval played is a major seventh, turns red if not
         checkAnswer(sender: sender, interval: 11)
-
+        
     }
-
+    
     @IBAction func octave(sender: UIButton){
         // Button turns green if the interval played is an octave, turns red if not
         checkAnswer(sender: sender, interval: 12)
-
+        
     }
-
+    
 
     /*
     // MARK: - Navigation
@@ -196,4 +224,3 @@ class DescendingDiatonicViewController: UIViewController {
     */
 
 }
-
